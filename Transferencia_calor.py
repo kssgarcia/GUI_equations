aeb73847556a910b1ded26a41b0533be611c3972
unknown <kssgarcia@outlook.com>
2021-03-31 11:09:07 -0500

    cambio a radiacion

--- Transferencia_calor.py
+++ Transferencia_calor.py
@@ -1,66 +1,49 @@
 from sympy import symbols, Eq, solve
-# -----------------------------------------------------------Reynolds----------------------------------
+
+# -----------------------------------------------------------Calor Radiacion----------------------------------
 
 
-class Muros:
-    Q = 0
-    T1 = 0
-    T2 = 1
-    conduccion = 0
-    convec = 0
+class Radiacion:
     incog = 0
 
-    def __init__(self, L, k, A):
-        self.L = L
-        self.k = k
+    def __init__(self, q, A, T1, T2, E):
+        self.q = q
         self.A = A
-        if Muros.Q == None:
-            Muros.Q = symbols('Q')
-            Muros.incog = Muros.Q
-        if self.L == None:
-            self.L = symbols('L')
-            Muros.incog = self.L
-        elif self.k == None:
-            self.k = symbols('k')
-            Muros.incog = self.k
+        self.T1 = T1
+        self.T2 = T2
+        self.E = E
+        if self.q == None:
+            self.q = symbols('q')
+            Radiacion.incog = self.q
         elif self.A == None:
             self.A = symbols('A')
-            Muros.incog = self.A
-        Muros.conduccion += (self.L) / (self.k*self.A)
+            Radiacion.incog = self.A
+        elif self.T1 == None:
+            self.T1 = symbols('T1')
+            Radiacion.incog = self.T1
+        elif self.T2 == None:
+            self.T2 = symbols('T2')
+            Radiacion.incog = self.T2
+        elif self.E == None:
+            self.E = symbols('E')
+            Radiacion.incog = self.E
 
-    @classmethod
-    def conveccion(cls, h, Ac):
-        cls.h = h
-        cls.Ac = Ac
-        if cls.h == None:
-            cls.h = symbols('h')
-            Muros.incog = cls.h
-        elif cls.Ac == None:
-            cls.Ac = symbols('Ac')
-            Muros.incog = cls.Ac
-        cls.convec += 1 / (cls.h*cls.Ac)
-        return cls.convec
+    def solucion(self, solv=None):
 
-    def Solucion(self):
-        ecuacion = Eq((Muros.T1 - Muros.T2) /
-                      (Muros.conduccion + Muros.convec)-Muros.Q, 0)
-        return (Muros.incog, solve(ecuacion, Muros.incog)[0])
+        ecuacion = Eq(self.E*(5.669E-8)*self.A*((self.T1**4)-(self.T2**4)), self.q)
+        self.solv, *_ = solve(ecuacion, Radiacion.incog)
+        return f'El valor de {Radiacion.incog} es de {self.solv}'
+
 
     @classmethod
     def __repr__(cls):
-        return 'Ejemplo de uso:\n\tMuros.Q = 15500\n\tMuros.T1 = 25\n\tMuros.T2 = 50\n\tMuros.conveccion(h=1000, Ac=None)\n\tmuro_1 = Muros(L=50/1000, k=1, A=1)\n\tmuro_2 = Muros(L=50/1000, k=1, A=0.5)\n\tmuro_3 = Muros(L=50/1000, k=1, A=0.01)\n\tMuros.conveccion(h=500, Ac=1)\n\tprint(muro_3.Solucion())'
+        return 'Ejemplo de uso:\n\tRey_1 = Reynolds(Um=10, d=0.0254, U=2.57E-5)\n\tRey_1.Densidad(P=2*1.0132E5, R=287, T=473)\n\tprint(Rey_1.solucion())'
 
 
 if __name__ == '__main__':
-    Muros.Q = 15500
-    Muros.T1 = 25
-    Muros.T2 = 50
-    Muros.conveccion(h=1000, Ac=None)
-    muro_1 = Muros(L=50/1000, k=1, A=1)
-    # muro_2 = Muros(L=50/1000, k=1, A=0.5)
-    # muro_3 = Muros(L=50/1000, k=1, A=0.01)
-    # Muros.conveccion(h=500, Ac=1)
-    print(muro_1.Solucion())
+    Radia_1 = Radiacion(q=None, A=5, T1=564, T2=515, E=1)
+    print(Radia_1.solucion())
+
 # -----------------------------------------------------------Reynolds----------------------------------
 
 
@@ -86,89 +69,90 @@ class Reynolds:
             Reynolds.incog = self.U
 
     def Densidad(self, P, R, T):
-        if type(self.Re) == type(symbols('x')):
-            self.P = P
-            self.R = R
-            self.T = T
-            if self.P == None:
-                self.P = symbols('P')
-                Reynolds.incog = self.P
-            elif self.R == None:
-                self.R = symbols('R')
-                Reynolds.incog = self.R
-            self.densidad = P/(R*T)
-            return self.densidad
+
+        self.P = P
+        self.R = R
+        self.T = T
+        if self.P == None:
+            self.P = symbols('P')
+            Reynolds.incog = self.P
+        elif self.R == None:
+            self.R = symbols('R')
+            Reynolds.incog = self.R
+        self.densidad = P/(R*T)
+        return self.densidad
 
     def solucion(self, solv=None):
-        if type(self.Re) == type(symbols('x')):
-            ecuacion = Eq(((self.densidad*self.Um*self.d)/self.U)-self.Re, 0)
-            self.solv = solve(ecuacion, Reynolds.incog)[0]
-            return f'El valor de {Reynolds.incog} es de {self.solv}'
-        else:
-            self.solv = self.Re
-            return f'El valor de Re es de {self.solv}'
+
+        ecuacion = Eq(((self.densidad*self.Um*self.d)/self.U)-self.Re, 0)
+        self.solv = solve(ecuacion, Reynolds.incog)[0]
+        return f'El valor de {Reynolds.incog} es de {self.solv}'
+
 
     @classmethod
     def __repr__(cls):
         return 'Ejemplo de uso:\n\tRey_1 = Reynolds(Um=10, d=0.0254, U=2.57E-5)\n\tRey_1.Densidad(P=2*1.0132E5, R=287, T=473)\n\tprint(Rey_1.solucion())'
 
 
-# if __name__ == '__main__':
-#     Rey_1 = Reynolds(Re=None, Um=10, d=0.0254, U=2.57E-5)
-#     Rey_1.Densidad(P=2*1.0132E5, R=287, T=473)
-#     Rey_1.solucion()
-#     print(Rey_1.solucion())
+if __name__ == '__main__':
+    Rey_1 = Reynolds(Re=5151, Um=512, d=None, U=2.57E-5)
+    Rey_1.Densidad(P=2*1.0132E5, R=287, T=473)
+    print(Rey_1.solucion())
+    
+
+
+    
 # -----------------------------------------------------------Nusselt----------------------------------
 
 
-class Nusselt(Reynolds):
-    incog = 0
+# class Nusselt(Reynolds):
+#     incog = 0
 
-    def __init__(self, Re, Um, d, U, h, k, Pr, n):
-        super().__init__(Re, Um, d, U)
-        self.h = h
-        self.k = k
-        self.Pr = Pr
-        self.n = n
-        if self.h == None:
-            self.h = symbols('h')
-            Nusselt.incog = self.h
-        elif self.k == None:
-            self.k = symbols('k')
-            Nusselt.incog = self.k
-        elif self.Pr == None:
-            self.Pr = symbols('Pr')
-            Nusselt.incog = self.Pr
-        elif self.n == None:
-            self.n = symbols('n')
-            Nusselt.incog = self.n
+#     def __init__(self, Re, Um, d, U, h, k, Pr, n):
+#         super().__init__(Re, Um, d, U)
+#         self.h = h
+#         self.k = k
+#         self.Pr = Pr
+#         self.n = n
+#         if self.h == None:
+#             self.h = symbols('h')
+#             Nusselt.incog = self.h
+#         elif self.k == None:
+#             self.k = symbols('k')
+#             Nusselt.incog = self.k
+#         elif self.Pr == None:
+#             self.Pr = symbols('Pr')
+#             Nusselt.incog = self.Pr
+#         elif self.n == None:
+#             self.n = symbols('n')
+#             Nusselt.incog = self.n
 
-    def NPr(self):
-        N_Pr = (0.023*(self.solv**0.8)*self.Pr**self.n)
-        return N_Pr
+#     def NPr(self):
+#         N_Pr = (0.023*(self.solv**0.8)*self.Pr**self.n)
+#         return N_Pr
 
-    def Nh(self):
-        N_h = (self.h*self.d)/self.k
-        return N_h
+#     def Nh(self):
+#         N_h = (self.h*self.d)/self.k
+#         return N_h
 
-    def solucion_total(self):
-        ecuacion = Eq(self.NPr()-self.Nh(), 0)
-        self.solv = solve(ecuacion, Nusselt.incog)[0]
-        return f'El valor de {Nusselt.incog} es de {self.solv}'
+#     def solucion_total(self):
+#         ecuacion = Eq(self.NPr()-self.Nh(), 0)
+#         self.solv = solve(ecuacion, Nusselt.incog)[0]
+#         return f'El valor de {Nusselt.incog} es de {self.solv}'
 
-    @classmethod
-    def __repr__(cls):
-        return 'Ejemplo de uso:\n\tRey_1 = Reynolds(Um=10, d=0.0254, U=2.57E-5)\n\tRey_1.Densidad(P=2*1.0132E5, R=287, T=473)\n\tprint(Rey_1.solucion())'
+#     @classmethod
+#     def __repr__(cls):
+#         return 'Ejemplo de uso:\n\tRey_1 = Reynolds(Um=10, d=0.0254, U=2.57E-5)\n\tRey_1.Densidad(P=2*1.0132E5, R=287, T=473)\n\tprint(Rey_1.solucion())'
 
 
-# if __name__ == '__main__':
-#     nuss_1 = Nusselt(Re=None, Um=10, d=0.0254, U=2.57E-5,
-#                      h=None, k=0.0386, Pr=0.681, n=0.4)
-#     # nuss_1 = Nusselt(Re=14753.0811949218, Um=10, d=0.0254, U=2.57E-5,
-#     #                  h=None, k=0.0386, Pr=0.681, n=0.4)
-#     nuss_1.Densidad(P=2*1.0132E5, R=287, T=473)
-#     print(nuss_1.solucion())
+# # if __name__ == '__main__':
+# #     nuss_1 = Nusselt(Re=None, Um=10, d=0.0254, U=2.57E-5,
+# #                      h=None, k=0.0386, Pr=0.681, n=0.4)
+# #     # nuss_1 = Nusselt(Re=14753.0811949218, Um=10, d=0.0254, U=2.57E-5,
+# #     #                  h=None, k=0.0386, Pr=0.681, n=0.4)
+# #     nuss_1.Densidad(P=2*1.0132E5, R=287, T=473)
+# #     print(nuss_1.solucion())
 
-#     nuss_1.NPr()
-#     nuss_1.Nh()
-#     print(nuss_1.solucion_total())
+# #     nuss_1.NPr()
+# #     nuss_1.Nh()
+# #     print(nuss_1.solucion_total())
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class GUI(tk.Frame):
         self.master = master
         self.pack()
         # create the button and set the command
-        self.muros = tk.Button(self, text="Muros", command=self.managed_windows)
+        self.radiacion = tk.Button(self, text="Radiacion", command=self.managed_windows)
         self.reynolds = tk.Button(self, text="Reynolds")
         self.nusselt = tk.Button(self, text="Nusselt")
         self.quit = tk.Button(self, text="Quit", command=self.master.destroy)
@@ -17,7 +17,7 @@ class GUI(tk.Frame):
         self.Grid_set()
 
     def Grid_set(self):
-        self.muros.grid(row=0, column=0)
+        self.radiacion.grid(row=0, column=0)
         self.reynolds.grid(row=1, column=0)
         self.nusselt.grid(row=2, column=0)
         self.quit.grid(row=3, column=0)
@@ -25,10 +25,10 @@ class GUI(tk.Frame):
     def managed_windows(self):
         self.newWindow = tk.Toplevel(self.master)
         self.newWindow.geometry('500x500')
-        self.app = window_muros(self.newWindow)
+        self.app = window_radiacion(self.newWindow)
 
-
-class window_muros(tk.Frame):
+#---------------------------------------------Radiacion------------------------------------------
+class window_radiacion(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
@@ -43,50 +43,40 @@ class window_muros(tk.Frame):
     def labels_pictures(self):
         # create the button and set the command
         # Title
-        tk.Label(self, text='MUROS',).grid(row=0, column=1, columnspan=2)
+        tk.Label(self, text='Radiacion').grid(row=0, column=1, columnspan=2)
         # Images
-        tk.Label(self, image=self.muro1).grid(row=1, column=0, columnspan=2)
-        tk.Label(self, image=self.muro2).grid(row=1, column=3, columnspan=2)
-        # labels for entries
-        tk.Label(self, text='Q').grid(row=3, column=0)
-        tk.Label(self, text='T1').grid(row=4, column=0)
-        tk.Label(self, text='T2').grid(row=5, column=0)
-        # conveccion
-        tk.Label(self, text='h').grid(row=6, column=0)
-        tk.Label(self, text='Ac').grid(row=7, column=0)
-        # para muros
-        tk.Label(self, text='L').grid(row=8, column=0)
-        tk.Label(self, text='k').grid(row=9, column=0)
-        tk.Label(self, text='A').grid(row=10, column=0)
+        tk.Label(self, image=self.radia1).grid(row=1, column=0, columnspan=2)
+        # Variables
+        tk.Label(self, text='q').grid(row=3, column=0)
+        tk.Label(self, text='E').grid(row=4, column=0)
+        tk.Label(self, text='A').grid(row=5, column=0)
+        tk.Label(self, text='T1').grid(row=6, column=0)
+        tk.Label(self, text='T2').grid(row=7, column=0)
         # button to q
         tk.Button(self, text="Quit", command=self.master.destroy).grid(row=11,column=0)
 
 
     def Entries(self):
         # labels for entries
-        self.Q = tk.Entry(self, text='Q')
-        self.Q.grid(row=3, column=1, columnspan=2)
-        self.T1 = tk.Entry(self, text='T1')
-        self.T1.grid(row=4, column=1, columnspan=2)
-        self.T2 = tk.Entry(self, text='T2')
-        self.T2.grid(row=5, column=1, columnspan=2)
-        # conveccion
-        self.h = tk.Entry(self, text='h')
-        self.h.grid(row=6, column=1, columnspan=2)
-        self.Ac = tk.Entry(self, text='Ac')
-        self.Ac.grid(row=7, column=1, columnspan=2)
-        # para muros
-        self.L = tk.Entry(self, text='L')
-        self.L.grid(row=8, column=1, columnspan=2)
-        self.k = tk.Entry(self, text='k')
-        self.k.grid(row=9, column=1, columnspan=2)
-        self.A = tk.Entry(self, text='A')
-        self.A.grid(row=10, column=1, columnspan=2)
+        self.q = tk.Entry(self)
+        self.q.grid(row=3, column=1, columnspan=2)
+
+        self.E = tk.Entry(self)
+        self.E.grid(row=4, column=1, columnspan=2)
+
+        self.A = tk.Entry(self)
+        self.A.grid(row=5, column=1, columnspan=2)
+
+        self.T1 = tk.Entry(self)
+        self.T1.grid(row=6, column=1, columnspan=2)
+
+        self.T2 = tk.Entry(self)
+        self.T2.grid(row=7, column=1, columnspan=2)
+    
 
 
     def get_entries(self):
-        self.dict_entries = {'Q': self.Q.get(), 'T1': self.T1.get(), 'T2': self.T2.get(), 'h': self.h.get(), 'Ac': self.Ac.get(), 'L': self.L.get()\
-            , 'k': self.A.get(), 'A': self.A.get()}
+        self.dict_entries = {'q': self.q.get(), 'E': self.E.get(), 'A': self.A.get(), 'T1': self.T1.get(), 'T2': self.T2.get()}
         for item, value in self.dict_entries.items():
             if value == 'None':
                 self.dict_entries[item] = None
@@ -97,31 +87,22 @@ class window_muros(tk.Frame):
 
     def Buttons(self):
         save = tk.Button(self, text='Save', command=self.get_entries)
+        show = tk.Button(self, text='Show', command=self.show)
         save.grid(row=10, column=4)
+        show.grid(row=10, column=5)
 
 
     def images(self):
         # image 1
-        self.muro1 = Image.open("images\\muros1.PNG")
-        self.muro1 = self.muro1.resize((200, 200))
-        self.muro1 = ImageTk.PhotoImage(self.muro1)
-        # image 2
-        self.muro2 = Image.open("images\\muros2.PNG")
-        self.muro2 = self.muro2.resize((200, 200))
-        self.muro2 = ImageTk.PhotoImage(self.muro2)
+        self.radia1 = Image.open("images\\radia1.PNG")
+        self.radia1 = self.radia1.resize((200, 200))
+        self.radia1 = ImageTk.PhotoImage(self.radia1)
 
+    def equation(self, q, E, A, T1, T2):
+        self.Radia_1 = Radiacion(q=q, A=A, T1=T1, T2=T2, E=E)
 
-    def equation(self, Q, T1, T2, h, Ac, L, k, A):
-        Muros.Q = Q
-        Muros.T1 = T1
-        Muros.T2 = T2
-        Muros.conveccion(h=h, Ac=Ac)
-        muro_1 = Muros(L=L/1000, k=k, A=A)
-        # muro_2 = Muros(L=L/1000, k=k, A=A)
-        # muro_3 = Muros(L=L/1000, k=k, A=A)
-        # Muros.conveccion(h=h, Ac=Ac)
-        print(muro_1.Solucion())
-
+    def show(self):
+        tk.Label(self, text=f'{self.Radia_1.solucion()}').grid(row=9, column=4)
 
 master = tk.Tk()
 master.geometry('200x150')
