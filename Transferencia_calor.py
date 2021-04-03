fa197834cc6bcccd9b0175c8e395e25a24f558e0
ehernandes21 <ehernandes@eafit.edu.co>
2021-04-02 22:10:00 -0500

    Se modificaron las ecuaciones de radiacion

--- Transferencia_calor.py
+++ Transferencia_calor.py
@@ -2,7 +2,6 @@ from sympy import symbols, Eq, solve
 
 # -----------------------------------------------------------Calor Radiacion----------------------------------
 
-
 class Radiacion:
     incog = 0
 
@@ -35,69 +34,68 @@ class Radiacion:
         return f'El valor de {Radiacion.incog} es de {self.solv}'
 
 
-    @classmethod
-    def __repr__(cls):
-        return 'Ejemplo de uso:\n\tRey_1 = Reynolds(Um=10, d=0.0254, U=2.57E-5)\n\tRey_1.Densidad(P=2*1.0132E5, R=287, T=473)\n\tprint(Rey_1.solucion())'
+    # @classmethod
+    # def __repr__(cls):
+    #     return 'Ejemplo de uso:\n\tRey_1 = Reynolds(Um=10, d=0.0254, U=2.57E-5)\n\tRey_1.Densidad(P=2*1.0132E5, R=287, T=473)\n\tprint(Rey_1.solucion())'
 
 
-if __name__ == '__main__':
-    Radia_1 = Radiacion(q=None, A=5, T1=564, T2=515, E=1)
-    print(Radia_1.solucion())
+# if __name__ == '__main__':
+#     Radia_1 = Radiacion(q=None, A=5, T1=564, T2=515, E=1)
+#     print(Radia_1.solucion())
 
 # -----------------------------------------------------------Reynolds----------------------------------
 
+# class Reynolds:
+#     incog = 0
 
-class Reynolds:
-    incog = 0
+#     def __init__(self, Re, Um, d, U):
+#         self.Re = Re
+#         self.Um = Um
+#         self.d = d
+#         self.U = U
+#         if self.Re == None:
+#             self.Re = symbols('Re')
+#             Reynolds.incog = self.Re
+#         elif self.Um == None:
+#             self.Um = symbols('Um')
+#             Reynolds.incog = self.Um
+#         elif self.d == None:
+#             self.d = symbols('d')
+#             Reynolds.incog = self.d
+#         elif self.U == None:
+#             self.U = symbols('U')
+#             Reynolds.incog = self.U
 
-    def __init__(self, Re, Um, d, U):
-        self.Re = Re
-        self.Um = Um
-        self.d = d
-        self.U = U
-        if self.Re == None:
-            self.Re = symbols('Re')
-            Reynolds.incog = self.Re
-        elif self.Um == None:
-            self.Um = symbols('Um')
-            Reynolds.incog = self.Um
-        elif self.d == None:
-            self.d = symbols('d')
-            Reynolds.incog = self.d
-        elif self.U == None:
-            self.U = symbols('U')
-            Reynolds.incog = self.U
+#     def Densidad(self, P, R, T):
 
-    def Densidad(self, P, R, T):
+#         self.P = P
+#         self.R = R
+#         self.T = T
+#         if self.P == None:
+#             self.P = symbols('P')
+#             Reynolds.incog = self.P
+#         elif self.R == None:
+#             self.R = symbols('R')
+#             Reynolds.incog = self.R
+#         self.densidad = P/(R*T)
+#         return self.densidad
 
-        self.P = P
-        self.R = R
-        self.T = T
-        if self.P == None:
-            self.P = symbols('P')
-            Reynolds.incog = self.P
-        elif self.R == None:
-            self.R = symbols('R')
-            Reynolds.incog = self.R
-        self.densidad = P/(R*T)
-        return self.densidad
+#     def solucion(self, solv=None):
 
-    def solucion(self, solv=None):
-
-        ecuacion = Eq(((self.densidad*self.Um*self.d)/self.U)-self.Re, 0)
-        self.solv = solve(ecuacion, Reynolds.incog)[0]
-        return f'El valor de {Reynolds.incog} es de {self.solv}'
-
-
-    @classmethod
-    def __repr__(cls):
-        return 'Ejemplo de uso:\n\tRey_1 = Reynolds(Um=10, d=0.0254, U=2.57E-5)\n\tRey_1.Densidad(P=2*1.0132E5, R=287, T=473)\n\tprint(Rey_1.solucion())'
+#         ecuacion = Eq(((self.densidad*self.Um*self.d)/self.U)-self.Re, 0)
+#         self.solv = solve(ecuacion, Reynolds.incog)[0]
+#         return f'El valor de {Reynolds.incog} es de {self.solv}'
 
 
-if __name__ == '__main__':
-    Rey_1 = Reynolds(Re=5151, Um=512, d=None, U=2.57E-5)
-    Rey_1.Densidad(P=2*1.0132E5, R=287, T=473)
-    print(Rey_1.solucion())
+#     @classmethod
+#     def __repr__(cls):
+#         return 'Ejemplo de uso:\n\tRey_1 = Reynolds(Um=10, d=0.0254, U=2.57E-5)\n\tRey_1.Densidad(P=2*1.0132E5, R=287, T=473)\n\tprint(Rey_1.solucion())'
+
+
+# if __name__ == '__main__':
+#     Rey_1 = Reynolds(Re=5151, Um=512, d=None, U=2.57E-5)
+#     Rey_1.Densidad(P=2*1.0132E5, R=287, T=473)
+#     print(Rey_1.solucion())
     
 
 
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class GUI(ttk.Frame):
         self.master = master
         self.pack()
         # create the button and set the command
-        self.radiacion = ttk.Button(self, text="Radiacion", command=self.managed_windows)
+        self.radiacion = ttk.Button(self, text="Radiacion", command=self.sub_managed_windows)
         self.reynolds = ttk.Button(self, text="Reynolds")
         self.nusselt = ttk.Button(self, text="Nusselt")
         self.quit = ttk.Button(self, text="Quit", command=self.master.destroy)
@@ -24,12 +24,66 @@ class GUI(ttk.Frame):
         self.nusselt.grid(row=2, column=0)
         self.quit.grid(row=3, column=0)
 
+    
+    def sub_managed_windows(self):
+        self.newWindow = tk.Toplevel(self.master)
+        self.newWindow.geometry('200x200')
+        self.app = window1(self.newWindow)
+
+class window1(ttk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        # create the buttons and set the command
+        self.forradiacion = ttk.Button(self, text="Formula-Radicion", command=self.managed_windows)
+        self.radbblack = ttk.Button(self, text="Radiacion de cuerpo negro", command=self.body_black_radiation)
+        self.quit = ttk.Button(self, text="Quit", command=self.master.destroy)
+        # Grid the buttons
+        self.forradiacion.grid(row=0, column=0)
+        self.radbblack.grid(row=1, column=0)
+        self.quit.grid(row=2, column=0)
+    
     def managed_windows(self):
         self.newWindow = tk.Toplevel(self.master)
-        self.newWindow.geometry('500x500')
+        self.newWindow.geometry('600x500')
         self.app = window_radiacion(self.newWindow)
-
+    def body_black_radiation(self):
+        self.newWindow = tk.Toplevel(self.master)
+        self.newWindow.geometry('600x500')
+        self.app = cuerpo_negro(self.newWindow)
 #---------------------------------------------Radiacion------------------------------------------
+class cuerpo_negro(ttk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        # Images 
+        # self.imagenes()
+        # Widgets
+        self.labels_imagenes()
+        self.Entradas()
+        # self.Botones()
+    def labels_imagenes(self):
+        # Title
+        ttk.Label(self, text='Radiacion').grid(row=0, column=1, columnspan=2)
+        # Images
+        # ttk.Label(self, image=self.radia4).grid(row=1, column=0, columnspan=2)
+        #Variables
+        ttk.Label(self, text='Reflectividad [p]').grid(row=3, column=0)
+        ttk.Label(self, text='Absorbencia [o]').grid(row=4, column=0)
+        ttk.Label(self, text='Transmisividad [t]').grid(row=5, column=0)
+        #button
+        ttk.Button(self, text="Quit", command=self.master.destroy).grid(row=11,column=0)
+    def Entradas(self):
+        # labels for entries
+        self.p = ttk.Entry(self)
+        self.p.grid(row=3, column=1, columnspan=2)
+
+        self.o = ttk.Entry(self)
+        self.o.grid(row=4, column=1, columnspan=2)
+
+        self.t = ttk.Entry(self)
+        self.t.grid(row=5, column=1, columnspan=2)
+
 class window_radiacion(ttk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
@@ -48,12 +102,13 @@ class window_radiacion(ttk.Frame):
         ttk.Label(self, text='Radiacion').grid(row=0, column=1, columnspan=2)
         # Images
         ttk.Label(self, image=self.radia1).grid(row=1, column=0, columnspan=2)
+        ttk.Label(self, image=self.radia3).grid(row=1, column=2, columnspan=2)
         # Variables
-        ttk.Label(self, text='q').grid(row=3, column=0)
+        ttk.Label(self, text='q [W]').grid(row=3, column=0)
         ttk.Label(self, text='E').grid(row=4, column=0)
-        ttk.Label(self, text='A').grid(row=5, column=0)
-        ttk.Label(self, text='T1').grid(row=6, column=0)
-        ttk.Label(self, text='T2').grid(row=7, column=0)
+        ttk.Label(self, text='A [m2]').grid(row=5, column=0)
+        ttk.Label(self, text='T1 [K]').grid(row=6, column=0)
+        ttk.Label(self, text='T2 [K]').grid(row=7, column=0)
         # button to q
         ttk.Button(self, text="Quit", command=self.master.destroy).grid(row=11,column=0)
 
@@ -90,26 +145,58 @@ class window_radiacion(ttk.Frame):
     def Buttons(self):
         save = ttk.Button(self, text='Save', command=self.get_entries)
         show = ttk.Button(self, text='Show', command=self.show)
-        save.grid(row=10, column=4)
-        show.grid(row=10, column=5)
+        tbl = ttk.Button(self, text='Tables', command=self.managed_windows1)
+        save.grid(row=10, column=1)
+        show.grid(row=10, column=2)
+        tbl.grid(row=10, column=3)
 
 
     def images(self):
         # image 1
-        self.radia1 = Image.open("images\\radia1.PNG")
-        self.radia1 = self.radia1.resize((200, 200))
+        self.radia1 = Image.open("images\\radia2.JPG")
+        self.radia1 = self.radia1.resize((250, 200))
         self.radia1 = ImageTk.PhotoImage(self.radia1)
+        # Image 2
+        self.radia3 = Image.open("images\\radia3.JPG")
+        self.radia3 = self.radia3.resize((150, 200))
+        self.radia3 = ImageTk.PhotoImage(self.radia3)
 
     def equation(self, q, E, A, T1, T2):
         self.Radia_1 = Radiacion(q=q, A=A, T1=T1, T2=T2, E=E)
 
     def show(self):
-        ttk.Label(self, text=f'{self.Radia_1.solucion()}').grid(row=9, column=4)
+        ttk.Label(self, text=f'{self.Radia_1.solucion()}').grid(row=11, column=1)
+    
+    def managed_windows1(self):
+        self.newWindow1 = tk.Toplevel(self.master)
+        self.newWindow1.geometry('500x500')
+        self.app = tables(self.newWindow1)
 
+class tables(ttk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        # Images 
+        self.images()
+        # Labels
+        self.labels_pictures_tables()
+    def labels_pictures_tables(self):
+        # create the button and set the command
+        # Title
+        ttk.Label(self, text='Tables').grid(row=0, column=1, columnspan=2)
+        # Images
+        ttk.Label(self, image=self.radia2).grid(row=1, column=0)
+        # button to q
+        ttk.Button(self, text="Quit", command=self.master.destroy).grid(row=11,column=0)
+    def images(self):
+    # Table 1
+        self.radia2 = Image.open("images\\radia1.png")
+        self.radia2 = self.radia2.resize((400, 400))
+        self.radia2 = ImageTk.PhotoImage(self.radia2)
+    
 master = ThemedTk(themebg=True)
 #ubuntu
 master.set_theme('breeze')
 master.geometry('200x150')
 app = GUI(master=master)
 app.mainloop()
-
