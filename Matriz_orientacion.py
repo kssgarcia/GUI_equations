e9e53519d12c81e05b9ef4d9ce28276cb588c142
unknown <kssgarcia@outlook.com>
2021-04-07 11:15:00 -0500

    actualizacion de matriz

--- Matriz_orientacion.py
+++ Matriz_orientacion.py
@@ -5,12 +5,12 @@ from ttkthemes import ThemedTk
 
 
 class Tablas_transfe(ttk.Frame):
-    dicta = {'nuss_1': {'Flujo interno', 'Turbulento', 'Tubo liso', 'desarrollado', 'Propiedades fijas', None, 'Formula simple', 'Formula algebraica', 'Seccion tubo entrada',None},
-'nuss_2': {'Flujo interno', 'Turbulento', 'Tubo liso', 'desarrollado', 'Propiedades variables', None, 'Formula simple', 'Formula algebraica', 'Seccion tubo entrada',None},
-'nuss_3': {'Flujo interno', 'Turbulento', 'Tubo liso', 'desarrollado', 'Propiedades fijas', None, 'Formula simple', 'Formula algebraica', 'Seccion tubo entrada',None},
-'nuss_4': {'Flujo interno', 'Turbulento', 'rugoso', 'desarrollado', None, 'T calor constante', 'compleja',  'Formula algebraica', 'Seccion tubo entrada',None},
-'nuss_5': {'Flujo interno', 'Laminar', 'Tubo liso', 'desarrollado', None, 'T tubo constante', None,  None, 'Seccion tubo entrada',None},
-'nuss_6': {'Flujo interno', 'Laminar', None, None, 'Propiedades variables', 'T tubo constante', None, None, None,None}}
+    dicta = {'nuss_1': {'Flujo interno', 'Turbulento', 'Tubo liso', 'desarrollado', 'Propiedades fijas', 'None_6', 'Formula simple', 'Formula algebraica', 'Seccion tubo entrada','None_10'},
+'nuss_2': {'Flujo interno', 'Turbulento', 'Tubo liso', 'desarrollado', 'Propiedades variables', 'None_6', 'Formula simple', 'Formula algebraica', 'Seccion tubo entrada','None_10'},
+'nuss_3': {'Flujo interno', 'Turbulento', 'Tubo liso', 'desarrollado', 'Propiedades fijas', 'None_6', 'Formula simple', 'Formula algebraica', 'Seccion tubo entrada','None_10'},
+'nuss_4': {'Flujo interno', 'Turbulento', 'rugoso', 'desarrollado', 'None_5', 'T calor constante', 'compleja',  'Formula algebraica', 'Seccion tubo entrada','None_10'},
+'nuss_5': {'Flujo interno', 'Laminar', 'Tubo liso', 'desarrollado', 'None_5', 'T tubo constante', 'None_7',  'None_8', 'Seccion tubo entrada','None_10'},
+'nuss_6': {'Flujo interno', 'Laminar', 'None_3', 'None_5', 'Propiedades variables', 'T tubo constante', 'None_7', 'None_8', 'None_9','None_10'}}
 
     def __init__(self, master=None):
         super().__init__(master)
@@ -27,26 +27,26 @@ class Tablas_transfe(ttk.Frame):
         self.matriz_img = ImageTk.PhotoImage(self.matriz_img)
     
     def entries(self):
-        self.example_1 = ttk.Combobox(self, values=('Flujo interno','Externo',None))
+        self.example_1 = ttk.Combobox(self, values=('Flujo interno','Externo','None_1'))
         self.example_1.grid(row=3, column=0)
-        self.example_2 = ttk.Combobox(self, values=('Laminar','Turbulento',None))
+        self.example_2 = ttk.Combobox(self, values=('Laminar','Turbulento','None_2'))
         self.example_2.grid(row=4, column=0)
-        self.example_3 = ttk.Combobox(self, values=('Tubo liso','rugoso',None))
+        self.example_3 = ttk.Combobox(self, values=('Tubo liso','rugoso','None_3'))
         self.example_3.grid(row=5, column=0)
-        self.example_4 = ttk.Combobox(self, values=('No desarrollado','desarrollado',None))
+        self.example_4 = ttk.Combobox(self, values=('No desarrollado','desarrollado','None_4'))
         self.example_4.grid(row=6, column=0)
-        self.example_5 = ttk.Combobox(self, values=('Propiedades fijas','Propiedades variables',None))
+        self.example_5 = ttk.Combobox(self, values=('Propiedades fijas','Propiedades variables','None_5'))
         self.example_5.grid(row=7, column=0)
-        self.example_6 = ttk.Combobox(self, values=('T tubo constante','T calor constante',None))
+        self.example_6 = ttk.Combobox(self, values=('T tubo constante','T calor constante','None_6'))
         self.example_6.grid(row=8, column=0)
-        self.example_7 = ttk.Combobox(self, values=('Formula simple','compleja',None))
+        self.example_7 = ttk.Combobox(self, values=('Formula simple','compleja','None_7'))
         self.example_7.grid(row=9, column=0)
-        self.example_8 = ttk.Combobox(self, values=('Formula algebraica','Grafica',None))
-        self.example_8.grid(row=9, column=0)
-        self.example_9 = ttk.Combobox(self, values=('Seccion tubo entrada','a lo largo',None))
-        self.example_9.grid(row=10, column=0)
-        self.example_10 = ttk.Combobox(self, values=('Propiedades pelicula','global',None))
-        self.example_10.grid(row=11, column=0)
+        self.example_8 = ttk.Combobox(self, values=('Formula algebraica','Grafica','None_8'))
+        self.example_8.grid(row=10, column=0)
+        self.example_9 = ttk.Combobox(self, values=('Seccion tubo entrada','a lo largo','None_9'))
+        self.example_9.grid(row=11, column=0)
+        self.example_10 = ttk.Combobox(self, values=('Propiedades pelicula','global','None_10'))
+        self.example_10.grid(row=12, column=0)
 
 
     def get_entries(self):
@@ -59,6 +59,10 @@ class Tablas_transfe(ttk.Frame):
             if len(list(current)) < len(list(global_e)):
                 global_e = current
                 self.equat = key
+            elif 'rugoso' in self.combo_values:
+                self.equat = 'nuss_4'
+                break
+
 
     def Buttons(self):
         save = ttk.Button(self, text='Guardar', command=self.get_entries)
